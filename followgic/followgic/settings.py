d620d50fe977b842cfa11c2f5e13f6471d180021
sergioperez1998 <sergiopm150898@gmail.com>
2020-11-25 16:53:38 +0100

    #2 Conexión con la base de datos PostgreSQL creada

--- followgic/followgic/settings.py
+++ followgic/followgic/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'followgic.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'followgic',
+        'USER': 'user',
+        'PASSWORD': 'user',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
