441ddf0dbd1db17633dd3df8e64e2e27d0a62650
sergioperez1998 <sergiopm150898@gmail.com>
2020-12-06 20:53:58 +0100

    añadiendo commits

--- followgic/user/urls.py
+++ followgic/user/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('miPerfil/', views.verMiPerfil, name="miPerfil"),
-    path('', include('djoser.urls')),
+    #path('', include('djoser.urls')),
     path('', include('djoser.urls.authtoken')),
     path('logout/', views.logout.as_view(), name="logout"),
 ]
