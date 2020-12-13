3993f7eadf1088adbce6233cb50c096ae813ad56
sergioperez1998 <sergiopm150898@gmail.com>
2020-12-13 17:20:56 +0100

    Añadido el controlador para editar una imagen

--- followgic/user/serializers.py
+++ followgic/user/serializers.py
@@ -10,7 +10,12 @@ class MagoProfileSerializer(UserSerializer):
 class MagoCreateSerializer(UserCreateSerializer):
     class Meta(UserCreateSerializer.Meta):
         model = Mago
-        fields = ('username', 'password', 'email', 'telefono', 'nombre', 'nombre_artistico', 'descripcion', 'pagina_web', 'foto', 'modalidades')
+        fields = ('username', 'password', 'email', 'telefono', 'nombre', 'nombre_artistico', 'descripcion', 'pagina_web', 'modalidades')
+
+class FotoMagoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Mago
+        fields = ['foto']
 
 class ModalidadesSerializer(serializers.ModelSerializer):
     class Meta:
