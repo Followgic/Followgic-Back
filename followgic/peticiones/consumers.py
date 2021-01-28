b354c24458e68863df30923215142cd780b7f260
sergioperez1998 <sergiopm150898@gmail.com>
2021-01-28 16:45:42 +0100

    Real time backend

--- followgic/peticiones/consumers.py
+++ followgic/peticiones/consumers.py
@@ -1,46 +1,24 @@
-import json
-from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.generic.websocket import WebsocketConsumer
 
-class ChatConsumer(AsyncWebsocketConsumer):
+class ChatConsumer(WebsocketConsumer):
 
-    room_name = None
-    room_group_name = None
+    def connect(self):
+        # Nombre del usuario actual
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        # Creo una sala con el nombre del usuario actual
+        self.room_group_name = self.room_name
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name, self.channel_name)
+        self.accept()
 
-    async def connect(self):
-        self.room_name = self.scope['url_route']['kwargs']['id']
-        self.room_group_name = 'peticion_%s' % self.room_name
-
-        await self.channel_layer.group_add(
+    def disconnect(self, close_code):
+        # Leave room group
+        async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name,
-            self.channels_name
+            self.channel_name
         )
-        await self.accept()
-
-    async def disconnect(self):
-        if self.room_group_name and self.channel_name:
-            await self.channel_layer.group_discard(
-                self.room_group_name,
-                self.channel_name
-            ) 
-
-    async def receive(self, text_data= None, bytes_data=None):
-        event = json.loads(text_data)
-        message = event['message']
-
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'peticion',
-                'username': self.scope['user'].username.name,
-                'message': message
-            }
-        )
-
-    async def chat_message(self, event):
-        message = event['message']
-        username = event['username']
-
-        await self.send(text_data=json.dumps({
-            'message': message,
-            'username': username
-        }))
+    # broadcast Notification; Individual + community
+        def broadcast_notification_message(self, event):
+            message = event['message']
+            self.send(text_data=json.dumps({'message': message
+                                            }))
--- followgic/peticiones/models.py
+++ followgic/peticiones/models.py
@@ -2,24 +2,41 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db.models.signals import post_save
 
+
 class Peticion(models.Model):
-    estado = models.IntegerField(verbose_name='Estado', validators=[MinValueValidator(-1), MaxValueValidator(1)])
+    estado = models.IntegerField(verbose_name='Estado', validators=[
+                                 MinValueValidator(-1), MaxValueValidator(1)])
     fecha = models.DateField(verbose_name='Fecha de creación')
-    remitente = models.ForeignKey('user.Mago', on_delete=models.CASCADE, related_name='remitente', null=True)
-    destinatario = models.ForeignKey('user.Mago', on_delete=models.CASCADE, related_name='destinatario', null=True)
-    
+    remitente = models.ForeignKey(
+        'user.Mago', on_delete=models.CASCADE, related_name='remitente', null=True)
+    destinatario = models.ForeignKey(
+        'user.Mago', on_delete=models.CASCADE, related_name='destinatario', null=True)
+
     def __str__(self):
         return "Petición de amistad de " + self.remitente.nombre + " a " + self.destinatario.nombre
-    
+
     class Meta:
         ordering = ('fecha', )
 
+
 def crear_grupo_notificacion(sender, instance, **kwargs):
+    # Conseguimos la peticion
     id_peticion = instance.pk
-    peticion = Peticion.objects.get(pk = id_peticion)
+    peticion = Peticion.objects.get(pk=id_peticion)
+
     if(peticion.estado == 0):
         print(peticion)
-    else:
-        pass
+        channel_layer = get_channel_layer()
+        # Nombre del grupo // peticion_sergio
+        nombre_grupo = "peticion_{}".format(peticion.destinatario.username)
 
-post_save.connect(crear_grupo_notificacion, sender= Peticion)
+        async_to_sync(channel_layer.group_send)(
+            nombre_grupo, {"type": "broadcast_notification_message",
+                           "message": peticion.__str__
+                           }
+        )
+    else:
+        print('No se ha creado la peticion de amistad')
+
+
+post_save.connect(crear_grupo_notificacion, sender=Peticion)
