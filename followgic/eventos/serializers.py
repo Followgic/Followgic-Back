54e4e850db82629d1091eb1e4102900515a01315
sergioperez1998 <sergiopm150898@gmail.com>
2021-04-18 16:58:46 +0200

    añadiendo id en los listar de evento

--- followgic/eventos/serializers.py
+++ followgic/eventos/serializers.py
@@ -14,7 +14,7 @@ class listarEventoSerializer(serializers.ModelSerializer):
     localizacion = crearLocalizacionSerializer(many=False)
     class Meta:
         model = Evento
-        fields = ['pk', 'titulo', 'tipo', 'privacidad', 'link_conferencia', 'descripcion', 'fecha_evento', 'hora_evento', 'aforo', 'foto', 'modalidades', 'comentarios', 'localizacion']
+        fields = ['id', 'titulo', 'tipo', 'privacidad', 'link_conferencia', 'descripcion', 'fecha_evento', 'hora_evento', 'aforo', 'foto', 'modalidades', 'comentarios', 'localizacion']
 
 class crearComentarioSerializer(serializers.ModelSerializer):
     class Meta:
