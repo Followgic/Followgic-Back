7051323cf31da21d52cddd737b4926a4c7443fe2
sergioperez1998 <sergiopm150898@gmail.com>
2021-05-22 14:18:38 +0200

    more settings

--- followgic/followgic/asgi.py
+++ followgic/followgic/asgi.py
@@ -7,8 +7,6 @@ import tiempo_real.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'followgic.settings')
 
-channel_layer = get_channel_layer()
-
 application = ProtocolTypeRouter({
   "http": get_asgi_application(),
   # Just HTTP for now. (We can add other protocols later.)
