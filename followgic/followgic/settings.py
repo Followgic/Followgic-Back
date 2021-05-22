c580680660a0f4ebe1a3d4059673f5acc2462c36
sergioperez1998 <sergiopm150898@gmail.com>
2021-05-22 17:35:07 +0200

    Menos mal?

--- followgic/followgic/settings.py
+++ followgic/followgic/settings.py
@@ -74,7 +74,8 @@ MIDDLEWARE = [
 
 CORS_ORIGIN_WHITELIST = (
   'http://localhost:4200',
-  'http://127.0.0.1:4200'
+  'http://127.0.0.1:4200',
+  'https://followgic.azurewebsites.net'
 )
 
 ROOT_URLCONF = 'followgic.urls'
