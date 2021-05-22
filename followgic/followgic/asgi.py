1ed28c9664b1e2e444c9878ab251092af8b9d0e7
sergioperez1998 <sergiopm150898@gmail.com>
2021-05-22 16:36:08 +0200

    mas configuraciones

--- followgic/followgic/asgi.py
+++ followgic/followgic/asgi.py
@@ -2,18 +2,19 @@ import os
 
 import django
 from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.routing import ProtocolTypeRouter, URLRouter, get_default_application
 from django.core.asgi import get_asgi_application
 import tiempo_real.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'followgic.settings')
 django.setup()
-application = ProtocolTypeRouter({
-  "http": get_asgi_application(),
-  # Just HTTP for now. (We can add other protocols later.)
-  "websocket": AuthMiddlewareStack(
-        URLRouter(
-            tiempo_real.routing.websocket_urlpatterns
-        )
-    ),
-})
+# application = ProtocolTypeRouter({
+#   "http": get_asgi_application(),
+#   # Just HTTP for now. (We can add other protocols later.)
+#   "websocket": AuthMiddlewareStack(
+#         URLRouter(
+#             tiempo_real.routing.websocket_urlpatterns
+#         )
+#     ),
+# })
+application = get_default_application()
