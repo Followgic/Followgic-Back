f67bb96e8f6f89ad0dc4b3ecf847d839232cc1c0
sergioperez1998 <sergiopm150898@gmail.com>
2021-05-22 16:42:44 +0200

    probando

--- Procfile
+++ Procfile
@@ -2,5 +2,5 @@
 release: cd followgic && python manage.py migrate
 % especifica el comando para lanzar Decide
 % web: cd followgic && gunicorn followgic.wsgi --log-file -
-web: cd followgic && daphne followgic.asgi:application --port $PORT --bind 0.0.0.0 -v2
+web: cd followgic && daphne followgic.asgi:application
 % chatworker: cd followgic && python manage.py runworker --settings=followgic.settings -v2
--- followgic/followgic/asgi.py
+++ followgic/followgic/asgi.py
@@ -8,13 +8,13 @@ import tiempo_real.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'followgic.settings')
 django.setup()
-# application = ProtocolTypeRouter({
-#   "http": get_asgi_application(),
-#   # Just HTTP for now. (We can add other protocols later.)
-#   "websocket": AuthMiddlewareStack(
-#         URLRouter(
-#             tiempo_real.routing.websocket_urlpatterns
-#         )
-#     ),
-# })
-application = get_asgi_application()
+
+application = ProtocolTypeRouter({
+  "http": get_asgi_application(),
+  # Just HTTP for now. (We can add other protocols later.)
+  "websocket": AuthMiddlewareStack(
+        URLRouter(
+            tiempo_real.routing.websocket_urlpatterns
+        )
+    ),
+})
