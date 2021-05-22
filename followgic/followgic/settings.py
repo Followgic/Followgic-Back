eec91ada44798e45aec1d05adb2fc8ef30e5ba89
sergioperez1998 <sergiopm150898@gmail.com>
2021-05-22 18:00:56 +0200

    Eliminando comentarios de los settings

--- followgic/followgic/settings.py
+++ followgic/followgic/settings.py
@@ -26,17 +26,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'hvlf=hep@j$x=8#05pro@(&27#r&sz(08v+-(6ejl=atwu_9@^'
 SECRET_KEY = config('SECRET_KEY', default='hvlf=hep@j$x=8#05pro@(&27#r&sz(08v+-(6ejl=atwu_9@^')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = False
 DEBUG = config('DJANGO_DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 BASEURL = config('BASEURL', default='')
-#BASEURL = 'https://followgic-backend.herokuapp.com'
-#ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else ['127.0.0.1']
 
 # Application definition
 
@@ -123,17 +119,6 @@ CACHES = {
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'followgic',
-#         'USER': 'user',
-#         'PASSWORD': 'user',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
 
 DATABASES = {
     'default': dj_database_url.config(
