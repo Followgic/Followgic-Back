431ff1e7ba0492dd4f3f1832ca758ae1a27ffc34
sergioperez1998 <sergiopm150898@gmail.com>
2021-05-27 18:39:29 +0200

    nuevo despliegue

--- followgic/followgic/settings.py
+++ followgic/followgic/settings.py
@@ -184,7 +184,6 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_ROOT = os.path.join(BASE_DIR,'carga/imagenes')
