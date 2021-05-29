d0f7f34652d6fde4358102e83d6590d7343402da
sergioperez1998 <sergiopm150898@gmail.com>
2021-05-29 18:16:52 +0200

    cambiando conf

--- followgic/followgic/settings.py
+++ followgic/followgic/settings.py
@@ -182,9 +182,10 @@ USE_L10N = True
 
 USE_TZ = True
 
-MEDIA_ROOT = os.path.join(BASE_DIR,'carga/imagenes')
-MEDIA_URL ='/carga/imagenes/'
-STATIC_URL = '/static/'
+# MEDIA_ROOT = os.path.join(BASE_DIR,'carga/imagenes')
+# MEDIA_URL ='/carga/imagenes/'
+STATIC_URL = '/carga/imagenes/'
+STATIC_ROOT = os.path.join(BASE_DIR,'carga/imagenes')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 db_from_env = dj_database_url.config(conn_max_age=500)
