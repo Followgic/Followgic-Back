7b1c7fc34f0e7ba34cd7d42ccb448a27ad98e101
sergioperez1998 <sergiopm150898@gmail.com>
2021-06-03 17:49:49 +0200

    quitando comentarios

--- followgic/followgic/settings.py
+++ followgic/followgic/settings.py
@@ -149,11 +149,6 @@ DJOSER = {
 
 }
 
-
-
-# Password validation
-# https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
