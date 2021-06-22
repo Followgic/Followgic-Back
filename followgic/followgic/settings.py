cafde2af9606a655092b276df440e7ad637f2722
sergioperez1998 <sergiopm150898@gmail.com>
2021-06-22 19:18:36 +0200

    direccionando a redis local

--- followgic/followgic/settings.py
+++ followgic/followgic/settings.py
@@ -93,7 +93,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('followgic-redis.westeurope.azurecontainer.io', 6379)],
+            "hosts": [('127.0.0.1', 6379)],
         },
     },
 }
